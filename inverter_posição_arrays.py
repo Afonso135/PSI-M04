6c2e4f4175e6f5f5094b382ed2e07d61a8c255d1
Alpha17 <afonsopedrao2008@gmail.com>
2024-12-12 12:55:01 +0000

    12/12/24

--- inverter_posição_arrays.py
+++ inverter_posição_arrays.py
@@ -3,5 +3,14 @@ invertido=np.zero(5)
 NR_elementos=5
 for i in range(5):
  nomes=np.empty(NR_elementos,dtype="U20")
- nomes[i]=input("introduza um nome")
- 
+nomes[i]=input("introduza um nome")
+#criar um array
+nomes_invertidos=np.empty(NR_elementos),dtype="U20"
+#preencher o array invertendo as suas posições
+for i in range(NR_elementos):
+ k=NR_elementos-1
+ nomes_invertidos[k]=nomes[i]
+ k=k-1
+
+ #mostrar os dois arrays
+print(nomes,nomes_invertidos)
